80424a232a0c9b7ff9bd0d9d0d77217a68d4cf03
hongkunyoo <hongkunyoo@gmail.com>
2017-05-22 02:33:35 +0000

    add print_total_size

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ def main():
         crawl(args.num)
     elif args.c == 'dump':
         dump()
+    elif args.c == 'size':
+        print_total_size()
     else:
         print_total_len()
 
@@ -121,6 +123,14 @@ def print_total_len():
     s = storage.Storage()
     count = (len([i for i in s.get_entities()]))
     print(count)
+
+def print_total_size():
+    s = storage.Storage()
+    count = 0
+    for i in s.get_entities():
+        count += sys.getsizeof(i)
+    print(count)
+
 
 
 def dump():
